b5fe5caa444599b7f23bf60dee6db9455a4d2d89
anastasasenov <anastasasenov@hotmail.com>
2026-07-23 13:32:07 +0300

    added tests

--- test.c
+++ test.c
@@ -3,9 +3,18 @@
 #include <stdint.h>
 #include <inttypes.h> 
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
 
 #define NUM_OF_T 11
 #define NUM_OF_RN ( (uint64_t)1 << 10 )
+#define NUM_OF_SAMPLES 100000
+#define NUM_BUCKETS 10
+
 
 TThreadRaceRNG stRng;
 uint64_t random_value = 0;
@@ -57,10 +66,52 @@ int test_perf() {
     return 0;
 }
 
-int test_normal_distribution() {
-    
-    printf("\n\t2. Normal distribution test ...\n");
+int test_xi_sq() {
 
+    TThreadRaceRNG rngLoc;
+
+    printf("\n\t2. Xi-sq distribution test ...\n");
+
+    thread_race_rng_init( &rngLoc, time(NULL) ); /* seed */
+
+    double xi_squared = 0.0;
+    uint32_t counts[NUM_BUCKETS];
+    double expected = (double)NUM_OF_SAMPLES / NUM_BUCKETS; /* expected rates */
+
+    /* init */
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+
+        counts[i] = 0;
+    }
+    
+    /* count */
+    for (int i = 0; i < NUM_OF_SAMPLES; i++) {
+
+        int number = (thread_race_rng_next( &rngLoc ) % NUM_BUCKETS);
+        counts[number] ++;
+    }
+
+    // Xi2 = Sum( (O_i - E_i)^2 / E_i )
+    for (int i = 0; i < NUM_BUCKETS; i++) {
+
+        double observed = (double)counts[i];
+        double diff = observed - expected;
+        xi_squared += (diff * diff) / expected;
+    }
+
+    thread_race_rng_deinit( &rngLoc );
+
+    const double critical_value = 16.919;
+    printf("\t\t\Xi_squared: %f ( critical_value: %f )\n", xi_squared, critical_value);
+    assert (xi_squared < critical_value);
+
+    return 0;
+}
+
+int test_serial() {
+    
+    printf("\n\t3. Serial test ( Testing Pairs/Independence ) ...\n");
+    
     return 0;
 }
 
@@ -71,7 +122,8 @@ int main() {
     thread_race_rng_init( &stRng, 314159 );
 
     int nRet = test_perf()
-        + test_normal_distribution();
+        + test_xi_sq()
+        + test_serial();
     
     thread_race_rng_deinit( &stRng );
 
@@ -79,3 +131,4 @@ int main() {
     
     return nRet;
 }
+
